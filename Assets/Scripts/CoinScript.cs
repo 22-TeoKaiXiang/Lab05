77740ec476b455c8eb07525eee3197146246fc3d
23-TeoKaiXiang <chaengsss@hotmail.com>
2022-10-06 10:24:46 +0800

    Patch Notes 1.0
    Added game win/lose scenes
    
    added GUIScript and CoinScript
    
    Applied collision

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public float rotatespeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotatespeed * Time.deltaTime, 0, 0));
+    }
+}
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag.Equals("Coin"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
